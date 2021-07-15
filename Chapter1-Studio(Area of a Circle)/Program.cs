3646bc336f00885d0e4b6af6568e6f339fa17144
Rowan Polster <rowandownriver94@gmail.com>
2021-07-14 20:02:23 -0500

    Add project files.

---
+++ Chapter1-Studio(Area of a Circle)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Chapter1_Studio_Area_of_a_Circle_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double radius;
+            double area;
+            double circumference;
+            double diameter;
+            double mpg;
+            double amntOfGas;
+
+
+            Console.WriteLine("Please enter a radius: ");
+            input = Console.ReadLine();
+
+            radius = double.Parse(input);
+
+            Console.WriteLine("Please enter your car's MPG: ");
+
+            input = Console.ReadLine();
+
+            mpg = double.Parse(input);
+
+            area = Math.Round(Math.PI * Math.Pow(radius, 2.0), 2);
+            circumference = Math.Round((2 * Math.PI * radius), 2);
+            diameter = Math.Round((2 * radius), 2);
+            amntOfGas = Math.Round((circumference/mpg), 2);
+
+
+            Console.WriteLine("The area of a circle of radius " + radius + "is " + area);
+            Console.WriteLine("The circumference of a circle of radius " + radius + "is " + circumference);
+            Console.WriteLine("The diameter of a circle of radius " + radius + "is " + diameter);
+            Console.WriteLine("Amount of gas consumed to drive " + circumference + "is " + amntOfGas);
+        }
+    }
+}
